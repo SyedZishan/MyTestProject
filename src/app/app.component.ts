805e35a3df62ec1a86ff01eaf483dae0ab2cad1e
Your Name <you@example.com>
2018-10-10 22:05:13 +0530

    First and final commit hopefully

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
-
+import { Component ,OnInit} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'assignment';
+  gifSearchForm : FormGroup;
+  public data = [];
+
+  constructor(private fb : FormBuilder, 
+              private http : HttpClient  
+    ) { }
+
+
+  ngOnInit() {
+    this.initForm();
+    this.pageLoad();
+  }
+
+
+  initForm(){
+    this.gifSearchForm = this.fb.group({
+     searchTerm : ['',[]],
+     category : ['Show All',[]]
+    });    
+  }
+  
+  public search(){
+    let searchTerm = this.gifSearchForm.controls.searchTerm.value;
+    let apiKey = 'NClx2IdLUCFW9q9YKRDEaarB9X3xAm3z';
+    let url = 'http://api.giphy.com/v1/gifs/search?q='+ searchTerm + '&api_key=' + apiKey + '&limit=20';
+    this.http.get<any>(url).subscribe(response=>{
+        this.data = response.data;
+    });
+  }
+
+  public pageLoad(){
+    let searchTerm = this.gifSearchForm.controls.searchTerm.value;
+    let apiKey = 'NClx2IdLUCFW9q9YKRDEaarB9X3xAm3z';
+    let url = 'http://api.giphy.com/v1/gifs/trending?&api_key=' + apiKey + '&limit=20';
+    this.http.get<any>(url).subscribe(response=>{
+        this.data = response.data;        
+    });
+  }
+
 }
+
+
+
